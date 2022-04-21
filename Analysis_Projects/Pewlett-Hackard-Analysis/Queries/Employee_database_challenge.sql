f347603593f7bc177033d370a87ff8e0a157cdb1
krishnakanchi <kkanchi@krishnakanchis-MacBook-Pro.local>
2022-04-21 11:54:37 -0400

    count for the retiring titles

--- Analysis_Projects/Pewlett-Hackard-Analysis/Queries/Employee_database_challenge.sql
+++ Analysis_Projects/Pewlett-Hackard-Analysis/Queries/Employee_database_challenge.sql
@@ -1,15 +1,11 @@
-
+--checking the table contents in each of the existing tables
 SELECT * FROM departments;
-
 SELECT * FROM employee;
 SELECT * FROM dept_manager;
-
 SELECT * FROM salaries;
-
 SELECT * FROM titles;
-
 SELECT * FROM departmentemployee;
-	
+--create a retires title table and order	
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -23,8 +19,7 @@ ON e.emp_no = ti.emp_no
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no;
 
--- Use Dictinct with Orderby to remove duplicate rows
-
+-- Use Dictinct with Orderby to remove duplicate rows based on emp_no
 
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
@@ -37,6 +32,16 @@ INTO Unique_titles
 FROM retirement_titles as rt
 WHERE rt.to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
+
+--number of employees by the recent title who retire
+SELECT COUNT(title) FROM Unique_titles;
+
+SELECT u.title,
+COUNT(u.emp_no)
+INTO retiring_titles_count
+FROM unique_titles as u
+GROUP BY u.title
+ORDER BY COUNT(u.emp_no) DESC;
 
 --select the employee names whose birthdate is between 1952-1955
 
